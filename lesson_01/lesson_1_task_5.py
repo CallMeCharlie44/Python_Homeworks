b1ecb5b49bed32a18053d18bf67d7f2444bd45ab
CallMeCharlie44 <khataeva22@gmail.com>
2025-09-30 15:15:15 +0300

    Отправила и закомитила файлы

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def show_num(num):
+    print(num, end="")
+
+
+show_num(8)
+show_num(8)
+show_num(0)
+show_num(0)
+show_num(5)
+show_num(5)
+show_num(5)
+show_num(3)
+show_num(5)
+show_num(3)
+show_num(5)
