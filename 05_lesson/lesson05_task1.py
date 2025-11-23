f76e5805e7d58f96a21ffcccbd971f8191b0edc4
CallMeCharlie44 <khataeva22@gmail.com>
2025-11-23 16:12:56 +0300

    Новые файлы отправлены и закоммитены

---
+++ 05_lesson/lesson05_task1.py
@@ -0,0 +1,18 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://google.com")
+driver.get("http://uitestingplayground.com/classattr")
+
+sleep(3)
+
+blue_button_input = driver.find_element(By.CSS_SELECTOR, ".btn-primary" )
+blue_button_input.click()
+
+sleep(3)
+driver.quit()
