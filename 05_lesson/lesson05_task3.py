6d8d50e4936d87f887d8cc2b6b3acc42dbf8f7b6
CallMeCharlie44 <khataeva22@gmail.com>
2025-11-27 16:31:35 +0300

    Исправлены ошибки

--- 05_lesson/lesson05_task3.py
+++ 05_lesson/lesson05_task3.py
@@ -1,10 +1,11 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+service = FirefoxService(executable_path=GeckoDriverManager().install)
+driver = webdriver.Firefox(service=service)
 
 driver.get("http://the-internet.herokuapp.com/inputs")
 
