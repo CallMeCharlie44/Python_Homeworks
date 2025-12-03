ac6e008a286f24975bc9dc813e9d72a19c93e2d1
CallMeCharlie44 <khataeva22@gmail.com>
2025-12-03 19:33:05 +0300

    Добавлены файлы

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+options = Options()
+options.add_argument("--start-maximized")
+
+service = EdgeService("/Users/dartcharlie/Downloads/edgedriver_mac64/msedgedriver")
+driver = webdriver.Edge(service=service, options=options)
+
+
+def test_first_name():
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+    driver.find_element(By.NAME, 'first-name').send_keys('Иван')
+    driver.find_element(By.NAME, 'last-name').send_keys('Петров')
+    driver.find_element(By.NAME, 'address').send_keys('Ленина, 55-3')
+    driver.find_element(By.NAME, 'e-mail').send_keys('test@skypro.com')
+    driver.find_element(By.NAME, 'phone').send_keys('+7985899998787')
+    driver.find_element(By.NAME, 'zip-code').send_keys('')
+    driver.find_element(By.NAME, 'city').send_keys('Москва')
+    driver.find_element(By.NAME, 'country').send_keys('Россия')
+    driver.find_element(By.NAME, 'job-position').send_keys('QA')
+    driver.find_element(By.NAME, 'company').send_keys('SkyPro')
+    driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+
+zip_code_field = driver.find_element(By.ID, 'zip-code')
+assert 'alert-danger' in zip_code_field.get_attribute('class')
+
+green_fields = [
+        'first-name', 'last-name', 'address', 'e-mail', 'phone',
+        'city', 'country', 'job-position', 'company']
+
+for field_id in green_fields:
+        field = driver.find_element(By.ID, field_id)
+        assert 'alert-success' in field.get_attribute('class')
+
+driver.quit()
+
+
+
+
+
+
---
+++ 06K_lesson/test_03_shop.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+
+def test_shop():
+    driver = webdriver.Firefox(
+        service=FirefoxService(GeckoDriverManager().install()))
+    driver.get("https://www.saucedemo.com/")
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+
+    driver.find_element(By.ID, "login-button").click()
+
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+
+    driver.find_element(By.CSS_SELECTOR, "a.shopping_cart_link").click()
+
+    driver.find_element(By.ID, "checkout").click()
+
+    driver.find_element(By.ID, "first-name").send_keys("Анастасия")
+    driver.find_element(By.ID, "last-name").send_keys("Васильева")
+    driver.find_element(By.ID, "postal-code").send_keys("428038")
+
+    driver.find_element(By.ID, "continue").click()
+
+    element = driver.find_element(By.CSS_SELECTOR, "div.summary_total_label")
+    total_price = element.text
+
+    print(f"Проверка успешна! Итоговая сумма равна {total_price}")
+
+driver.quit()
